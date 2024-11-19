fcab7711a837f4c269ad9eefcabdcc1f7e9e182a
Hussien2605 <168656898+Hussien2605@users.noreply.github.com>
2024-11-19 15:23:03 +0200

    verify

--- src/test/java/ac/il/bgu/qa/TestLibrary.java
+++ src/test/java/ac/il/bgu/qa/TestLibrary.java
@@ -10,9 +10,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
-import org.mockito.Spy;
-
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
